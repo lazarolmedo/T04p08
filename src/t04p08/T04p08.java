4dc7d7b146ec1d47d186d74e08690a5ef34fd903
Lalo <1902764@alu.murciaeduca.es>
2023-03-29 18:46:08 +0200

    v2.0

--- src/t04p08/T04p08.java
+++ src/t04p08/T04p08.java
@@ -3,7 +3,10 @@ package t04p08;
 public class T04p08 {
 
     public static void main(String[] args) {
+
         System.out.println("Hola GitHub");
+
+        System.out.println("v2.0");
     }
 
 }
